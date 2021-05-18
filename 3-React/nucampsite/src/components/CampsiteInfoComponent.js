0330f3b4081d706b44daba4c1a79936ddeb4f132
ChristianRodarmel <christian.rodarmel1@gmail.com>
2021-05-18 12:28:37 -0700

    Presentation and Container Components added, more of redesign to code structure

--- 3-React/nucampsite/src/components/CampsiteInfoComponent.js
+++ 3-React/nucampsite/src/components/CampsiteInfoComponent.js
@@ -34,28 +34,28 @@ renderComments(comments) {
                         {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
                         <p>{comment.date}</p>
                     </div>
-                )
+                );
             })}
 
             </div>
-        )
+        );
     }
+    return <div />
 }
 
     render() {
         if(this.props.campsite) {
                 return(
-                    <div className="row">
-                    {this.renderCampsite(this.props.campsite)}
-                    {this.renderComments(this.props.campsite.comments)}
+                    <div className="container">
+                        <div className="row">
+                            {this.renderCampsite(this.props.campsite)}
+                            {this.renderComments(this.props.campsite.comments)}
+                        </div>
                     </div>
-                )    
+                );    
             }
-            return(
-                <div>
-                    
-                </div>
-        );
+        return <div />;
+        
     }
 }
 
