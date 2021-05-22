91deba292fab0877c45c48885fc2294ab9fb4b6f
ChristianRodarmel <christian.rodarmel1@gmail.com>
2021-05-22 00:07:41 -0700

    Adding to the Single Page App

--- 3-React/nucampsite/src/components/MainComponent.js
+++ 3-React/nucampsite/src/components/MainComponent.js
@@ -4,21 +4,34 @@ import CampsiteInfo from "./CampsiteInfoComponent";
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { CAMPSITES } from '../shared/campsites';
+import { COMMENTS } from '../shared/comments';
+import { PARTNERS } from '../shared/partners';
+import { PROMOTIONS } from '../shared/promotions';
 
 class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
             campsites: CAMPSITES,
+            comments: COMMENTS,
+            partners: PARTNERS,
+            promotions: PROMOTIONS
         };
     }
 
   render() {
+      // We use arrow function because of inherent 'this' keyword in arrow function
       const HomePage = () => {
           return (
-              <Home />
+              <Home 
+                campsite={this.state.campsites.filter(campsite => campsite.featured)[0]}
+                promotion={this.state.promotions.filter(promotion => promotion.featured)[0]}
+                partner={this.state.partners.filter(partner => partner.featured)[0]}
+                comments={this.state.comments.filter(comments => comments.featured)[0]}
+              />
           );
       };
       return (
@@ -27,6 +40,7 @@ class Main extends Component {
                 <Switch>
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+                    <Route exact path='/contactus' component={Contact} />
                     <Redirect to='/home' />
                 </Switch>
               <Footer />
