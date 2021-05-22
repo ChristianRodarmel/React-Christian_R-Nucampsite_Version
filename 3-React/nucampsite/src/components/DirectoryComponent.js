b838c0701159cded192c7e3a778b40717e1989fe
ChristianRodarmel <christian.rodarmel1@gmail.com>
2021-05-21 23:30:54 -0700

    React Router Setup

--- 3-React/nucampsite/src/components/DirectoryComponent.js
+++ 3-React/nucampsite/src/components/DirectoryComponent.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
-function RenderDirectoryItem({campsite, onClick}) {
+function RenderDirectoryItem({campsite}) {
     return(
-        <Card onClick={() => onClick(campsite.id)}>
+        <Card>
             <CardImg width="100%" src={campsite.image} alt={campsite.name} />
             <CardImgOverlay>
                 <CardTitle>{campsite.name}</CardTitle>
@@ -18,7 +18,7 @@ function Directory(props) {
         return (
             // we put an id on line 13 for arrow function to pass the prop for campsite ID to run since Directory Component no longer runs the state.id
             <div key={campsite.id} className="col-md-5 m-1">
-                <RenderDirectoryItem campsite={campsite} onClick={props.onClick} />
+                <RenderDirectoryItem campsite={campsite} />
             </div>
         );
     });
