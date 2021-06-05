d857b0005c9b2600cf799013587f1afc5b2f4b13
ChristianRodarmel <christian.rodarmel1@gmail.com>
2021-06-05 09:36:25 -0700

    Fetch Handling Errors

--- 3-React/nucampsite/src/redux/promotions.js
+++ 3-React/nucampsite/src/redux/promotions.js
@@ -1,11 +1,17 @@
-import { PROMOTIONS } from '../shared/promotions';
+import * as ActionTypes from './ActionTypes';
 
 
 // we chose to capitalize here but not needed, also used an arrow func but stylistic choice (could use regular func if wanted) 
-export const Promotions = (state = PROMOTIONS, action) => {
+export const Promotions = (state = { isLoading: true, errMess: null, promotions: [] }, action) => {
     switch (action.type) {
-        // made it default because nothing to send through state yet
+        case ActionTypes.ADD_PROMOTIONS:
+            return {...state, isLoading: false, errMess: null, promotions: action.payload};
+        case ActionTypes.PROMOTIONS_LOADING:
+            return {...state, isLoading: true, errMess: null, promotions: []}
+        case ActionTypes.PROMOTIONS_FAILED:
+            return {...state, isLoading: false, errMess: action.payload};
         default:
         return state;
     }
+    // made it default because nothing to send through state yet
 };
